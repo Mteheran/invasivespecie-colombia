f8fd407833a5dfe44f22afb5a3723ed137987033
juanr.cifuentes <juanr.cifuentes@icloud.com>
2023-10-03 01:30:14 -0500

    Created a modal that displays the specie given as a query parameter

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,14 +1,43 @@
 import * as React from "react";
 import Searcher from '../../templates/searcher'
 import { SearcherContext } from "../../context";
-// import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import getInvasiveSpecie, { InvasiveSpecie } from "../../services/invasiveSpecie";
+import InvasiveSpecieModal from "./invasiveSpecieModal";
 
 function Main() {
-  // let { query, id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+  const [data, setData] = useState<InvasiveSpecie>();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    getInvasiveSpecie(id as string)
+      .then(data => {
+        if(id !== "")
+        {
+          setIsModalOpen(true)
+          setData(data)
+        }
+      })
+      .catch(error => console.error(error));
+  }, [id]);
 
   return (
     <SearcherContext.Provider value={{}}>
       <Searcher/>
+      <div>
+        {data && (
+        <InvasiveSpecieModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          data={data}
+        />
+      )}
+      </div>
     </SearcherContext.Provider>
     
   );
